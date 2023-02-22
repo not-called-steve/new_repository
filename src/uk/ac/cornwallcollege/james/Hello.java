35cefae7beac9d89027bc0cd225cc0b1ed28d284
not-called-steve <secondaccount@email.com>
2023-02-22 11:13:35 +0000

    take arguments

---
+++ src/uk/ac/cornwallcollege/james/Hello.java
@@ -0,0 +1,7 @@
+package uk.ac.cornwallcollege.james;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world! " + args[0] + " " + args[1]);
+    }
+}
--- src/uk/ac/cornwallcollege/james/hello.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.cornwallcollege.james;
-
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
