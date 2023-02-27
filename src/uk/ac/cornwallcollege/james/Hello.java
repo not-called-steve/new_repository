afacefd5ab47b36611e3c6cb95da04e85eba6c50
not-called-steve <secondaccount@email.com>
2023-02-27 15:01:23 +0000

    name arranger added

--- src/uk/ac/cornwallcollege/james/Hello.java
+++ src/uk/ac/cornwallcollege/james/Hello.java
@@ -2,6 +2,26 @@ package uk.ac.cornwallcollege.james;
 
 public class Hello {
     public static void main(String[] args) {
+        int[] something = {5, 6, 2};
+        String[] name = {"first second", "third fourth", "fifth sixth"};
+        int spacePosition;
+        String firstname;
+        String lastname;
+        String initials;
+        String secondInitial;
+        int i;
+        for (i = 0; i < name.length; i++) {
+            spacePosition = name[i].indexOf(' ');
+            firstname = name[i].substring(0, spacePosition);
+            lastname = name[i].substring(spacePosition + 1, name[i].length());
+            initials = String.valueOf(name[i].charAt(0));
+            secondInitial = String.valueOf(name[i].charAt(spacePosition + 1));
+            initials = initials.concat(secondInitial);
+            System.out.println(lastname + ' ' + firstname);
+            System.out.println(initials);
+        }
+        something[0] = something[1] * something[2];
         System.out.println("Hello world! " + args[0] + " " + args[1]);
+        System.out.println(something[0]);
     }
 }
