fa7a689e4f1fb4208ec74ae9e73ae880df172296
not-called-steve <secondaccount@email.com>
2023-02-27 15:37:03 +0000

    name arranger improved

--- src/uk/ac/cornwallcollege/james/Hello.java
+++ src/uk/ac/cornwallcollege/james/Hello.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class Hello {
     public static void main(String[] args) {
-        int[] something = {5, 6, 2};
         List <String> name = new ArrayList<>();
         List <String> revName = new ArrayList<>();
         name.add("first second");
@@ -15,17 +14,10 @@ public class Hello {
         int spacePosition;
         String firstname;
         String lastname;
-        String initials;
-        String secondInitial;
         for (var i : name) {
             spacePosition = i.indexOf(' ');
             firstname = i.substring(0, spacePosition);
             lastname = i.substring(spacePosition + 1, i.length());
-            initials = String.valueOf(i.charAt(0));
-            secondInitial = String.valueOf(i.charAt(spacePosition + 1));
-            initials = initials.concat(secondInitial);
-            //System.out.println(lastname + ", " + firstname);
-            //System.out.println(initials);
             firstname = firstname.concat(", ");
             firstname = firstname.concat(lastname);
             revName.add(firstname);
@@ -34,8 +26,5 @@ public class Hello {
         for(var i : revName) {
             System.out.println(i);
         }
-        something[0] = something[1] * something[2];
-        System.out.println("Hello world! " + args[0] + " " + args[1]);
-        System.out.println(something[0]);
     }
 }
